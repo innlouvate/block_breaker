657043655a7e4c57b6a2fb35cf23a9a7c25987f1
innlouvate <l.j.franklin85@googlemail.com>
2017-07-21 22:15:05 +0100

    add moving paddle

--- src/application/BlockBreakerPanel.java
+++ src/application/BlockBreakerPanel.java
@@ -2,12 +2,19 @@ package application;
 
 import javax.swing.JPanel;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
-public class BlockBreakerPanel extends JPanel {
+public class BlockBreakerPanel extends JPanel implements KeyListener {
     ArrayList<Block> blocks = new ArrayList<Block>();
+    Block paddle;
+    Thread thread;
+    Animate animate;
 
     BlockBreakerPanel() {
+        paddle = new Block(175, 480, 150, 25, "./images/paddle.png");
+
         for(int i = 0; i<8; i++) {
             blocks.add(new Block((i*60+2), 0, 60, 25, "./images/blue.png"));
         }
@@ -20,12 +27,45 @@ public class BlockBreakerPanel extends JPanel {
         for(int i = 0; i<8; i++) {
             blocks.add(new Block((i*60+2), 75, 60, 25, "./images/yellow.png"));
         }
+
+        addKeyListener(this);
+        setFocusable(true);
     }
 
     @Override
     public void paintComponent(Graphics g) {
+        super.paintComponent(g);
         for(Block b: blocks)
             b.draw(g, this);
+        paddle.draw(g, this);
+    }
+
+    public void update() {
+        repaint();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+            animate = new Animate(this);
+            thread = new Thread(animate);
+            thread.start();
+        }
+        if(e.getKeyCode() == KeyEvent.VK_LEFT && paddle.x > 0) {
+            paddle.x -= 15;
+        }
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT && paddle.x < (getWidth() - paddle.width)) {
+            paddle.x += 15;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
 
     }
 }
